6aa0ef27a44c84aa6d512869a1f3790e42ea786f
fahahaha <214177702@qq.com>
2020-05-25 21:28:09 +0800

    1

---
+++ gitdemo2/src/test/java/com/neusoft/gitdemo2/Gitdemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neusoft.gitdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitdemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
